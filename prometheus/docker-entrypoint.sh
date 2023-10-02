096c861a1a7501549ea40b80e84c9f87f88d5785
AndreyTheKnight <32400688+AndreyTheKnight@users.noreply.github.com>
2023-10-02 13:15:31 +0300

    Update docker-entrypoint.sh

--- prometheus/docker-entrypoint.sh
+++ prometheus/docker-entrypoint.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo "- targets: ['host.docker.internal:9100']" >> /defconf/etc/prometheus/prometheus.yml
 docker-defconf-init.sh
 
 exec prometheus --config.file=/etc/prometheus/prometheus.yml
