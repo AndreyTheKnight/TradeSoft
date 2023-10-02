b6cc1031f1f302977ec60c9e9764d58db485f06f
AndreyTheKnight <32400688+AndreyTheKnight@users.noreply.github.com>
2023-10-02 15:49:20 +0300

    Update docker-entrypoint.sh

--- mysql/docker-entrypoint.sh
+++ mysql/docker-entrypoint.sh
@@ -7,8 +7,6 @@ then
 fi
 mkdir /run/mysqld
 
-#exec mysqld --user=root --log-error=/var/log/mysql/error.log
-
 chown -R mysql:mysql /run/mysqld
 chown -R mysql:mysql /var/lib/mysql
 chown -R mysql:mysql /var/log/mysql
