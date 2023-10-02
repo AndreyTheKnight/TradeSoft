23beff7d8309a4ff6a3fbbbf3dbf6be449a64511
AndreyTheKnight <32400688+AndreyTheKnight@users.noreply.github.com>
2023-10-02 15:49:35 +0300

    Update docker-entrypoint.sh

--- grafana/docker-entrypoint.sh
+++ grafana/docker-entrypoint.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 docker-defconf-init.sh
-chown -R grafana:grafana /usr/share/grafana
 
+chown -R grafana:grafana /usr/share/grafana
 exec su-exec grafana grafana server --config /etc/grafana/grafana.ini --homepath /usr/share/grafana
